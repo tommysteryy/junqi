4726af0c85d50ecc96c10cf7e8b92b53376b3f1d
Tommy Xu <tommysteryy@outlook.com>
2022-12-31 04:05:38 -0800

    made Map for looking up card size

--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
 let blackCard: string;
 let redCard: string;
+const cardSizes: object = {
+    "gong": 1,
+    "pai": 2,
+    "lian": 3,
+    "ying": 4,
+    "tuan": 5,
+    "lv": 6,
+    "shi": 7,
+    "jun": 8,
+    "si": 9
+}
+    
+const cardIndex: Map<string, number> = new Map(Object.entries(cardSizes));
+
 
 function getCards(): void {
     blackCard = (document.getElementById("black-card") as HTMLInputElement).value;
@@ -15,7 +29,7 @@ function calculateResult(blackCard: string, redCard: string) {
     throw new Error("Function not implemented.");
 }
 
-function displayResult() {
+function displayResult(): void{
     throw new Error("Function not implemented.");
 }
 
